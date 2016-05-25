422a295c45a9690ae516ce9961729931c70683d4
CJT1105 <CJT1105@GMAIL.COM>
2016-05-25 11:45:23 -0500

    finished products page

---
+++ app.js
@@ -0,0 +1,80 @@
+var productArray = ['product1', 'product2', 'product3','product4','product5','product6','product7','product8'];
+var product1 = {name: "Baseball Glove Stolen from Neighbor Kid", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed blandit nulla, at finibus ligula. Nullam mi risus, sollicitudin a leo et, sagittis auctor lectus. Maecenas sit amet luctus purus, id bibendum diam. Nulla venenatis.", price: "$12.99",url: "http://www.cityguideny.com/uploads2/67410/Baseball-Boy-Glove-Closeup-MAIN.jpg"};
+var product2 = {name: "Stone Cold Autographed Beer Can", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed blandit nulla, at finibus ligula. Nullam mi risus, sollicitudin a leo et, sagittis auctor lectus. Maecenas sit amet luctus purus, id bibendum diam. Nulla venenatis.", price: "$200.00", url:"http://cdn2.business2community.com/wp-content/uploads/2015/08/steve-austin.vadapt.955.high_.0.jpg"};
+var product3 = {name: "Johnny Manziel's Failed Drug Test", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed blandit nulla, at finibus ligula. Nullam mi risus, sollicitudin a leo et, sagittis auctor lectus. Maecenas sit amet luctus purus, id bibendum diam. Nulla venenatis.", price: "$100.00", url:"http://favsize.com/img/m/3/4/m34fb4c9fc65be50c82b097fb6ed62c3ao0.jpg"};
+var product4 = {name: " Tom Brady Autographed Football", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed blandit nulla, at finibus ligula. Nullam mi risus, sollicitudin a leo et, sagittis auctor lectus. Maecenas sit amet luctus purus, id bibendum diam. Nulla venenatis.", price: "$300.00", url:"http://www.blackandgoldlions.com/wp-content/uploads/2015/01/football-flat-web.jpg"};
+var product5 = {name: "3 dozen Honus Wagner Cards", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed blandit nulla, at finibus ligula. Nullam mi risus, sollicitudin a leo et, sagittis auctor lectus. Maecenas sit amet luctus purus, id bibendum diam. Nulla venenatis.", price: "$15.00", url:"http://www.pophistorydig.com/wp-content/uploads/2008/06/1909-Honus-Wagner-BBcard-2-300.jpg"};
+var product6 = {name: "OJ Simpson's Black Leathe7 Gloves", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed blandit nulla, at finibus ligula. Nullam mi risus, sollicitudin a leo et, sagittis auctor lectus. Maecenas sit amet luctus purus, id bibendum diam. Nulla venenatis.", price: "$5,000", url:"https://lintvwtnh.files.wordpress.com/2014/06/oj-simpson-gloves-1995-ap.jpg?w=640"};
+var product7 = {name: "Bloody Goalie Mask", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed blandit nulla, at finibus ligula. Nullam mi risus, sollicitudin a leo et, sagittis auctor lectus. Maecenas sit amet luctus purus, id bibendum diam. Nulla venenatis.", price: "$20.00", url:"http://ecx.images-amazon.com/images/I/81x7N09dRsL._UY445_.jpg"};
+var product8 = {name: "Celebrity Curling Broom", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed blandit nulla, at finibus ligula. Nullam mi risus, sollicitudin a leo et, sagittis auctor lectus. Maecenas sit amet luctus purus, id bibendum diam. Nulla venenatis.", price: "$30.00", url:"https://upload.wikimedia.org/wikipedia/commons/f/f0/Curling_broom.png"};
+var productName1 = document.querySelector('#productName1');
+productName1.textContent = product1.name;
+var productImage1 = document.querySelector('#productImage1');
+productImage1.src = product1.url;
+var productPrice1 = document.querySelector('#price1');
+productPrice1.textContent = product1.price;
+var productDescription1 = document.querySelector('#productDescription1');
+productDescription1.textContent = product1.description
+
+var productName2 = document.querySelector('#productName2');
+productName2.textContent = product2.name;
+var productImage2 = document.querySelector('#productImage2');
+productImage2.src = product2.url;
+var productPrice2 = document.querySelector('#price2');
+productPrice2.textContent = product2.price;
+var productDescription2 = document.querySelector('#productDescription2');
+productDescription2.textContent = product2.description
+
+var productName3 = document.querySelector('#productName3');
+productName3.textContent = product3.name;
+var productImage3 = document.querySelector('#productImage3');
+productImage3.src = product3.url;
+var productPrice3 = document.querySelector('#price3');
+productPrice3.textContent = product3.price;
+var productDescription3 = document.querySelector('#productDescription3');
+productDescription3.textContent = product3.description
+
+var productName4 = document.querySelector('#productName4');
+productName4.textContent = product4.name;
+var productImage4 = document.querySelector('#productImage4');
+productImage4.src = product4.url;
+var productPrice4 = document.querySelector('#price4');
+productPrice4.textContent = product4.price;
+var productDescription4 = document.querySelector('#productDescription4');
+productDescription4.textContent = product4.description
+
+var productName5 = document.querySelector('#productName5');
+productName5.textContent = product5.name;
+var productImage5 = document.querySelector('#productImage5');
+productImage5.src = product5.url;
+var productPrice5 = document.querySelector('#price5');
+productPrice5.textContent = product5.price2
+var productDescription5 = document.querySelector('#productDescription5');
+productDescription5.textContent = product5.description
+
+var productName6 = document.querySelector('#productName6');
+productName6.textContent = product6.name;
+var productImage6 = document.querySelector('#productImage6');
+productImage6.src = product6.url;
+var productPrice6 = document.querySelector('#price6');
+productPrice6.textContent = product6.price;
+var productDescription6 = document.querySelector('#productDescription6');
+productDescription6.textContent = product6.description
+
+var productName7 = document.querySelector('#productName7');
+productName7.textContent = product7.name;
+var productImage7 = document.querySelector('#productImage7');
+productImage7.src = product7.url;
+var productPrice7 = document.querySelector('#price7');
+productPrice7.textContent = product7.price;
+var productDescription7 = document.querySelector('#productDescription7');
+productDescription7.textContent = product7.description
+
+var productName8 = document.querySelector('#productName8');
+productName8.textContent = product8.name;
+var productImage8 = document.querySelector('#productImage8');
+productImage8.src = product8.url;
+var productPrice8 = document.querySelector('#price8');
+productPrice8.textContent = product8.price;
+var productDescription8 = document.querySelector('#productDescription8');
+productDescription8.textContent = product8.description
